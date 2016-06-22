c9b64af34468b4db2d1b28c66056a00b77d4bbf0
Matt Weeks <m_weeks@hotmail.co.uk>
2016-06-22 08:28:05 +0100

    Add tweepy lib.

---
+++ src/mjw77-early/main.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+from secrets import *
+
+import sys
+sys.path.insert(0, 'libs')
+
+import tweepy
+
+import webapp2
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('Welcome to twittester!')
+
+class StreamHandler(webapp2.RequestHandler):
+    def get(self):
+        auth = tweepy.OAuthHandler(getConsumerKey(), getConsumerSecret())
+        auth.secure = True
+        auth.set_access_token(getAccessToken(), getAccessTokenSecret())
+
+        api = tweepy.API(auth)
+
+        # If the authentication was successful, you should
+        # see the name of the account print out
+        print(api.me().name)
+
+        self.response.write(secretTest() + api.me().name())
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler),
+    ('/stream', StreamHandler)
+], debug=True)
---
+++ src/mjw77-early/secrets.template.py
@@ -0,0 +1,14 @@
+def getAccessToken():
+	return "[required]"
+
+def getAccessTokenSecret():
+	return "[required]"
+
+def getConsumerKey():
+	return "[required]"
+
+def getConsumerSecret():
+	return "[required]"
+
+def secretTest():
+	return "Jimmy Bob"
